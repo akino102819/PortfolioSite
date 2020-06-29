fc8f467c8109bbad6569ef4060390ef671628336
akino102819 <k.akino1998@icloud.com>
2020-06-29 14:26:52 +0900

    mail contact

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,3 +1,5 @@
 class ContactController < ApplicationController
+  def new
 
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,17 +29,12 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-  
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                  587,
-    domain:               'gmail.com',
-    user_name:            'ki.akino1998@gmail.com',
-    password:             'Momigi28',
-    authentication:       'plain',
-    enable_starttls_auto:  true
-  }
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
