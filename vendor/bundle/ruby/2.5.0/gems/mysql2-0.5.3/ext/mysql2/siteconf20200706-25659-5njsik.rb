f9ec904cb0ef88de7e1fa022d028bc1a0e466904
akino102819 <k.akino1998@icloud.com>
2020-07-06 18:29:34 +0900

    bundle install

--- vendor/bundle/ruby/2.5.0/gems/mysql2-0.5.3/ext/mysql2/siteconf20200706-25659-5njsik.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20200706-25659-nq17oq"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
